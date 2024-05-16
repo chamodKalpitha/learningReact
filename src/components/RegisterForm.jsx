dd819fda260232e06932c0579a63145c550cd37d
chamodKalpitha <chamodlive@live.com>
2024-05-16 22:17:45 +0530

    leaning about useState

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,19 +1,54 @@
+import { useState } from "react";
+
 export function RegisterForm() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [displayName, setDisplayName] = useState("");
+
+  const isDisabled = !username || !password || !displayName;
+
   return (
     <form>
       <div>
         <label htmlFor="rusername">Username</label>
-        <input type="text" name="rusername" id="rusername" />
+        <input
+          type="text"
+          name="rusername"
+          id="rusername"
+          value={username}
+          onChange={(e) => {
+            setUsername((prevUsername) => (prevUsername = e.target.value));
+          }}
+        />
       </div>
       <div>
         <label htmlFor="rpassword">Password</label>
-        <input type="text" name="rpassword" id="rpassword" />
+        <input
+          type="text"
+          name="rpassword"
+          id="rpassword"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </div>
       <div>
         <label htmlFor="rdisplayname">Display Name</label>
-        <input type="text" name="rdisplayname" id="rdisplayname" />
+        <input
+          type="text"
+          name="rdisplayname"
+          id="rdisplayname"
+          value={displayName}
+          onChange={(e) => setDisplayName(e.target.value)}
+        />
       </div>
-      <button>Register</button>
+      <button disabled={isDisabled}>Register</button>
+      <div>
+        <span>username: {username}</span>
+        <br />
+        <span>password: {password}</span>
+        <br />
+        <span>display Name: {displayName}</span>
+      </div>
     </form>
   );
 }
